9b54d12ae60cd41a00b9f752ccf618f185b99d16
Lukasz Paczkowski <lukasz@unity3d.com>
2014-08-29 08:24:44 +0200

    Do not set the Debugger.Soft.Unity process id. This was a bit of a hack and we will be using "Debug Editor" moving forward.

--- MonoDevelop.UnityMode/StartupOptions.cs
+++ MonoDevelop.UnityMode/StartupOptions.cs
@@ -1,26 +1,14 @@
-using System;
 
 namespace MonoDevelop.UnityMode
 {
 	public static class StartupOptions
 	{
-		private static int unityProcessId;
-
 		static StartupOptions()
 		{
 			UnityRestServerUrl = "http://localhost:38000";
 		}
 
-		public static int UnityProcessId
-		{
-			get { return unityProcessId; }
-			set 
-			{ 
-				unityProcessId = value;
-				Debugger.Soft.Unity.Util.UnityProcessId = value;
-			}
-		}
-
+		public static int UnityProcessId { get; set; }
 		public static string UnityRestServerUrl { get; set; }
 	}
 }
