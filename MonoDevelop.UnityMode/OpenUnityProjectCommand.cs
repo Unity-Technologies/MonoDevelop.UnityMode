83336030f4ef02b158128450dd7718107ec99b5f
Lukasz <lukaszunity@users.noreply.github.com>
2015-11-26 13:55:23 +0100

    Added "Open Unity Project..." menu item in File menu. Removed "Debug Editor" menu item, replaced by new attach with play button feature. Clean-up formatting in Manifest.addin.xml.

---
+++ MonoDevelop.UnityMode/OpenUnityProjectCommand.cs
@@ -0,0 +1,22 @@
+﻿using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui.Dialogs;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+
+namespace MonoDevelop.UnityMode
+{
+	public class OpenUnityProjectCommand : CommandHandler
+	{
+		protected override void Run ()
+		{
+			var dlg = new OpenFileDialog (GettextCatalog.GetString ("Select Unity project folder"), Gtk.FileChooserAction.SelectFolder) {
+				TransientFor = IdeApp.Workbench.RootWindow
+			};
+			if (!dlg.Run ())
+				return;
+
+			var folder = dlg.SelectedFile;
+		}
+	}
+}
+
